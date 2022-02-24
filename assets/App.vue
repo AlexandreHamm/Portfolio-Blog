c714380437c999a5b706fd5b20f6413699fde911
ahamm-prequal2021 <a.hamm@codeur.online>
2022-02-24 16:48:30 +0100

    structure change

---
+++ assets/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import Header from './components/portfolio/Header.vue'
+import Links from './components/Links.vue'
+import Socials from './components/Socials.vue'
+</script>
+
+<template>
+  <section>
+    <div class="portfolio">
+      <div class="portfolio__block">
+        <Header />
+        <RouterView />
+      </div>
+      <Socials />
+    </div>
+    <Links />
+  </section>
+</template>
+
+<style lang="scss">
+
+#app{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+}
+section{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.portfolio{
+  position: relative;
+  width: 64%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  &__block{
+    width: 100%;
+    max-height: 150px;
+    margin: 2rem 0;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+  }
+}
+</style>
---
+++ assets/views/PortfolioView.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Projects from '../components/portfolio/Projects.vue'
+</script>
+
+<template>
+        <Projects />
+</template>
